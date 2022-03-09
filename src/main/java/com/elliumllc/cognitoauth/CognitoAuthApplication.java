d867665025362b7686542cfaf4d5283f420428ee
Arnold Perez <arnolddperez@gmail.com>
2022-03-08 18:36:19 -0700

    adding project files

---
+++ src/main/java/com/elliumllc/cognitoauth/CognitoAuthApplication.java
@@ -0,0 +1,13 @@
+package com.elliumllc.cognitoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitoAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitoAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elliumllc/cognitoauth/CognitoAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elliumllc.cognitoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognitoAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
